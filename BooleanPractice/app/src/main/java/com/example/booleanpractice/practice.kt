2f845d1506a8d35cb9933d86e9a0d78e6c23ff64
Abla <la_rabia@esi.dz>
2023-11-28 19:50:26 +0100

    first commit

---
+++ BooleanPractice/app/src/main/java/com/example/booleanpractice/practice.kt
@@ -0,0 +1,10 @@
+package com.example.booleanpractice
+
+
+fun main(){
+    val myTrue=true
+    val myFalse=false
+    println(myTrue||myFalse)
+    println(myTrue&&myFalse)
+    println(!myTrue)//hello
+}
