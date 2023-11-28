ebad12b7c37a8aa18f3b82a01f8716cad851b123
Abla <la_rabia@esi.dz>
2023-11-28 20:13:22 +0100

    age for praying exercice

--- BooleanPractice/app/src/main/java/com/example/booleanpractice/practice.kt
+++ BooleanPractice/app/src/main/java/com/example/booleanpractice/practice.kt
@@ -2,9 +2,18 @@ package com.example.booleanpractice
 
 
 fun main(){
-    val myTrue=true
+    /*val myTrue=true
     val myFalse=false
     println(myTrue||myFalse)
     println(myTrue&&myFalse)
-    println(!myTrue)//hello
+    println(!myTrue)//hello*/
+    /**If else & readln part**/
+    print("Enter your age : ")
+    var age = readln().toInt()
+    if (age<7)
+        println("you are still young to pray .")
+    else if(age<10)
+        println("you should pray")
+    else
+        println("you must pray ")
 }
