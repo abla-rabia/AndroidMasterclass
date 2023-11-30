4de95d74ff6131ef9d234220ec4bf48e0e39f52c
Abla <la_rabia@esi.dz>
2023-11-30 14:23:53 +0100

    added average Calculator function

--- FunctionsExercices/app/src/main/java/com/example/functionsexercices/FunctionExercices.kt
+++ FunctionsExercices/app/src/main/java/com/example/functionsexercices/FunctionExercices.kt
@@ -2,18 +2,22 @@ package com.example.functionsexercices
 
 
 fun main(){
-    var choice =1
+    /*var choice =1
     var name =""
     var sugarCount=0
+    var price=0
     while (choice==1){
         print("Name : ")
         name= readln()
         print("Number of sugar spoons : ")
         sugarCount= readln().toInt()
-        makeCoffee(name, sugarCount)
+        price=makeCoffee(name, sugarCount)
+        println("the price is : $price")
         print("Do you want more ? enter 1 to say yes :")
         choice= readln().toInt()
-    }
+    }*/
+    var numbers=listOf(1, 2, 3, 4, 5)
+    println("the averga is ${calculateAverage(numbers)}")
 
 }
 
@@ -21,7 +25,7 @@ fun main(){
 
 // Define Function
 
-fun makeCoffee(name:String,sugarCount : Int){
+fun makeCoffee(name:String,sugarCount : Int):Int{
 
     if (sugarCount==1 )
         println("Coffee with $sugarCount spoon of sugar for $name")
@@ -30,6 +34,11 @@ fun makeCoffee(name:String,sugarCount : Int){
     else
         println("Coffee with $sugarCount spoons of sugar for $name")
 
+    return (1000)
 
-
+}
+fun calculateAverage(numbers: List<Int>):Double{
+    var s=numbers.sum()
+    var average=s/numbers.size.toDouble()
+    return average
 }
