26163c3320d76bddd2223ea89ffb157bc856f986
Abla <la_rabia@esi.dz>
2023-11-30 13:52:57 +0100

    updated coffee App

--- FunctionsExercices/app/src/main/java/com/example/functionsexercices/FunctionExercices.kt
+++ FunctionsExercices/app/src/main/java/com/example/functionsexercices/FunctionExercices.kt
@@ -1,12 +1,35 @@
 package com.example.functionsexercices
 
+
 fun main(){
-    makeCoffee()
+    var choice =1
+    var name =""
+    var sugarCount=0
+    while (choice==1){
+        print("Name : ")
+        name= readln()
+        print("Number of sugar spoons : ")
+        sugarCount= readln().toInt()
+        makeCoffee(name, sugarCount)
+        print("Do you want more ? enter 1 to say yes :")
+        choice= readln().toInt()
+    }
+
 }
-fun makeCoffee(){
-    println("creme coffee")
-    println("Cappucino")
-    println("milk")
-    println("long coffee")
-    println("coffe & milk")
+
+
+
+// Define Function
+
+fun makeCoffee(name:String,sugarCount : Int){
+
+    if (sugarCount==1 )
+        println("Coffee with $sugarCount spoon of sugar for $name")
+    else if (sugarCount==0)
+        println("Coffee with no sugar for $name")
+    else
+        println("Coffee with $sugarCount spoons of sugar for $name")
+
+
+
 }
