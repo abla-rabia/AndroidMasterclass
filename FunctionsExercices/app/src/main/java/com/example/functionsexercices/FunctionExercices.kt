4231b33c0ded9d4de9338915428e37c08ee9d501
Abla <la_rabia@esi.dz>
2023-11-30 12:33:45 +0100

    added new directory (functions apps)

---
+++ FunctionsExercices/app/src/main/java/com/example/functionsexercices/FunctionExercices.kt
@@ -0,0 +1,12 @@
+package com.example.functionsexercices
+
+fun main(){
+    makeCoffee()
+}
+fun makeCoffee(){
+    println("creme coffee")
+    println("Cappucino")
+    println("milk")
+    println("long coffee")
+    println("coffe & milk")
+}
