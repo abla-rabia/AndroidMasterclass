aa94cd654b48975d7c961b891ce7dd582d9b5660
Abla <la_rabia@esi.dz>
2023-11-30 12:13:53 +0100

    Added while loop

--- RockPapperScissorsApp/app/src/main/java/com/example/rockpapperscissorsapp/RockPaperScissorsApp.kt
+++ RockPapperScissorsApp/app/src/main/java/com/example/rockpapperscissorsapp/RockPaperScissorsApp.kt
@@ -1,22 +1,27 @@
 package com.example.rockpapperscissorsapp
 
 fun main(){
-    println("Enter your choice : ")
-    var playerChoice= readln()
-    var randomNumper=(1..3).random()
-    var computerChoice=""
-    when{
-        randomNumper==1 -> computerChoice="Rock"
-        randomNumper==2 -> computerChoice="Paper"
-        randomNumper==3 -> computerChoice="Scissors"
+    var ok="1"
+    while (ok=="1") {
+        println("Enter your choice : ")
+        var playerChoice = readln()
+        var randomNumper = (1..3).random()
+        var computerChoice = ""
+        when {
+            randomNumper == 1 -> computerChoice = "Rock"
+            randomNumper == 2 -> computerChoice = "Paper"
+            randomNumper == 3 -> computerChoice = "Scissors"
+        }
+        println("Computer choice : $computerChoice")
+        var winner = when {
+            computerChoice == playerChoice -> "Tie"
+            computerChoice == "Rock" && playerChoice == "Scissors" -> "Computer"
+            computerChoice == "Scissors" && playerChoice == "Paper" -> "Computer"
+            computerChoice == "Paper" && playerChoice == "Rock" -> "Computer"
+            else -> "Player"
+        }
+        println("winner : $winner")
+        print("Vould you like to continue ? Enter 1 to continue :")
+        ok= readln()
     }
-    println("Computer choice : $computerChoice")
-    var winner=when{
-        computerChoice==playerChoice -> "Tie"
-        computerChoice=="Rock" && playerChoice=="Scissors" -> "Computer"
-        computerChoice=="Scissors" && playerChoice=="Paper" -> "Computer"
-        computerChoice=="Paper" && playerChoice=="Rock" -> "Computer"
-        else -> "Player"
-    }
-    println("winner : $winner")
 }
